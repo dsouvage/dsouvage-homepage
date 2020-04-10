04f0160eccbdd34f358bfef2a3c0d4844d08764f
Dylan <dsouvage@gmail.com>
2020-04-10 11:51:00 -0400

    implement eslint-airbnb settings

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
+/* eslint-disable react/jsx-filename-extension */
 import React, { useState, useEffect } from 'react';
 import Pages from './pages';
 
 function App() {
-	const [ user, setUser ] = useState(null);
-	useEffect(() => {
-		// grab resume items from gitconnected
-		fetch('https://gitconnected.com/v1/portfolio/firefelix').then((res) => res.json()).then((user) => {
-			setUser(user);
-		});
-	}, []);
-	if (!user) {
-		return <div />;
-	}
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    // grab resume items from gitconnected
+    fetch('https://gitconnected.com/v1/portfolio/firefelix').then((res) => res.json()).then((user) => {
+      setUser(user);
+    });
+  }, []);
+  if (!user) {
+    return <div />;
+  }
 
-	return <Pages user={user} />;
+  return <Pages user={user} />;
 }
 
 export default App;
