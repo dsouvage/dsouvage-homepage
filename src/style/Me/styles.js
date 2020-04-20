9074cdbd9788985d1ce08bc94f61418831705563
Dylan <dsouvage@gmail.com>
2020-04-20 17:07:27 -0400

    Test coverage 4 (#12)
    * increase test coverage, remove styles from test coverage
    
    * remove unnesecary code
    
    * remove service worker

--- src/style/Me/styles.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import styled from 'styled-components';
-
-export const ProfileLink = styled.li`
-  margin-right: 0.5rem;
-  margin-bottom: 0.5rem;
-  display: inline-block;
-  font-size: 18px;
-`;
