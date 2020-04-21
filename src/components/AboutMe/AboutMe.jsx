6649ec51c7d30063e15ad1c3a75f5ab96864d726
Dylan <dsouvage@gmail.com>
2020-04-20 21:57:26 -0400

    add pic of snowboarding

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -4,6 +4,13 @@ import PropTypes from 'prop-types';
 const AboutMe = ({ user, SectionTitle, Paragraph }) => (
   <div>
     <SectionTitle>About Me</SectionTitle>
+    <img
+      src="https://i.imgur.com/6kEYP9a.jpg"
+      alt="Dylan Souvage"
+      align="right"
+      height="200"
+      width="160"
+    />
     <Paragraph>{user.basics.summary}</Paragraph>
   </div>
 );
