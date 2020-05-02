d5d5ba8333426aae7a978c6b06516ed8f0e0ea11
Dylan <dsouvage@gmail.com>
2020-05-02 12:41:10 -0400

    improve maintainability by reducing code duplication

--- src/components/Involvement/Involvement.jsx
+++ src/components/Involvement/Involvement.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { WorkItem, WorkTitle, JobTitle } from '../../style/Work/styles';
+import ListExperience from '../ListExperience/ListExperience';
 
 const Involvement = ({ user, SectionTitle, Paragraph }) => (
   <div>
@@ -9,23 +10,12 @@ const Involvement = ({ user, SectionTitle, Paragraph }) => (
     <ul>
       {user.volunteer.map((work) => (
         <WorkItem key={Date.now()}>
-          <WorkTitle>{work.position}</WorkTitle>
-          <div>
-            <JobTitle>{work.organization}</JobTitle> <span>{work.location}</span>
-            <span> &sdot; </span>
-            <span>
-              {work.start.month}/{work.start.year} to {work.end.month}/{work.end.year}
-            </span>
-          </div>
-          <Paragraph>
-            <a href={work.website} rel="noopener noreferrer" target="_blank">
-              {work.website}
-            </a>
-          </Paragraph>
-          <Paragraph>Summary: {work.summary}</Paragraph>
-          {work.highlights.map((highlight) => (
-            <Paragraph key={Date.now()}>{highlight}</Paragraph>
-          ))}
+          <ListExperience
+            work={work}
+            Paragraph={Paragraph}
+            WorkTitle={WorkTitle}
+            JobTitle={JobTitle}
+          />
         </WorkItem>
       ))}
     </ul>
