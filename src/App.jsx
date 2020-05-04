aa37aefda2c3b1fd59c3e041e37107be27a543d1
Dylan <dsouvage@gmail.com>
2020-05-03 20:59:20 -0400

    ignore getResume fnction in test coverage

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import Pages from './pages';
 
+/* istanbul ignore next */
+function getResume(setUser) {
+  fetch('https://gitconnected.com/v1/portfolio/firefelix')
+    .then((res) => res.json())
+    .then((updateUser) => {
+      setUser(updateUser);
+    });
+}
+
 function App() {
   const [user, setUser] = useState(null);
   useEffect(() => {
     // grab resume items from gitconnected
-    fetch('https://gitconnected.com/v1/portfolio/firefelix')
-      .then((res) => res.json())
-      .then((updateUser) => {
-        setUser(updateUser);
-      });
+    /* istanbul ignore next */
+    getResume(setUser);
   }, []);
   if (!user) {
     return <div />;
   }
 
+  /* istanbul ignore next */
   return <Pages user={user} />;
 }
 
