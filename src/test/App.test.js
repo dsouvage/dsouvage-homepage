6940872236731691835018794f67891b29a5a128
Dylan <dsouvage@gmail.com>
2020-08-19 20:37:24 -0400

    change line endings, increase test coverage

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,9 +1,9 @@
+/* eslint-disable no-undef */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 
-// eslint-disable-next-line no-undef
 it('renders without crashing', () => {
   shallow(<App />);
 });
