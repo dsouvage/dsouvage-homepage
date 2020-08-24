4c394404b98f0e4b6ad60c9b81893becd6b876af
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-24 20:17:02 +0000

    Deploy firefelix/dsouvage-homepage to github.com/firefelix/dsouvage-homepage.git:gh-pages

---
+++ precache-manifest.777f467ba0b2d3d1d93c47ffbeeceaf6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e04d65808fba200c6fa3765a9816d499",
+    "url": "/dsouvage-homepage/index.html"
+  },
+  {
+    "revision": "7bf9fd8416c82a81a443",
+    "url": "/dsouvage-homepage/static/css/2.8730d5d4.chunk.css"
+  },
+  {
+    "revision": "3b4473358cabfe946cbd",
+    "url": "/dsouvage-homepage/static/css/main.5c5dc240.chunk.css"
+  },
+  {
+    "revision": "7bf9fd8416c82a81a443",
+    "url": "/dsouvage-homepage/static/js/2.51fbff7a.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/dsouvage-homepage/static/js/2.51fbff7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b4473358cabfe946cbd",
+    "url": "/dsouvage-homepage/static/js/main.4bdc657c.chunk.js"
+  },
+  {
+    "revision": "eb0100afaa6216a654ec",
+    "url": "/dsouvage-homepage/static/js/runtime-main.579a63a4.js"
+  },
+  {
+    "revision": "2a7a1179ecfa2823f0b9a8f28929430b",
+    "url": "/dsouvage-homepage/static/media/SoloPic.2a7a1179.jpg"
+  }
+]);
